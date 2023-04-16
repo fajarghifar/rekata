8c52b1cc7c1a660e1a3793b906eabd28f00a15e2
Fajar Ghifari <fajarghifari3@gmail.com>
2023-04-17 04:52:33 +0700

    fix : delete the card after opened

--- scripts/app.js
+++ scripts/app.js
@@ -13,26 +13,29 @@ const showMenu = (toggleId, navId) => {
 }
 showMenu('nav-toggle', 'nav-menu');
 
-function generateCard() {
+
+/*=============== GENERATE CARD ===============*/
+const generateCard = () => {
     const getLocalStorageData = localStorage.getItem("available_cards");
 
     if (JSON.parse(getLocalStorageData).length == 0) {
         const cardCategory = document.getElementById("card-category");
         const cardQuestion = document.getElementById("card-question");
         const cardImage = document.getElementById("card-image");
+
         cardCategory.innerHTML = 'Tidak ada kartu yang tersisa!:(';
         cardQuestion.innerHTML = 'Harap refresh halaman web untuk memulai ulang permainan!';
         cardImage.src = 'img/default.webp';
-        console.log('Habiss')
     } else {
         const cards = JSON.parse(getLocalStorageData);
-        const randomIndex = Math.floor(cards.length * Math.random())
+        const randomIndex = Math.floor(cards.length * Math.random());
 
-        showCard(cards, cards[randomIndex]);
+        showCard(cards, cards[randomIndex], randomIndex);
     }
 }
 
-function showCard(cards, card) {
+/*=============== SHOW THE CARD TO THE PAGE ===============*/
+const showCard = (cards, card, index) => {
     const cardTotal = document.getElementById("card-total");
     const cardImage = document.getElementById("card-image");
     const cardCategory = document.getElementById("card-category");
@@ -47,33 +50,37 @@ function showCard(cards, card) {
         cardQuestion.innerHTML = card.question;
     }, 4500);
 
-    // Delete the card after openned
-    cards.splice(card, 1);
+    // Delete the card after opened
+    cards.splice(index, 1);
     localStorage.setItem("available_cards", JSON.stringify(cards));
 }
 
-function randomCardAnimation(){
+/*=============== ADD ANIMATION TO THE CARD ===============*/
+const randomCardAnimation = () => {
     const cardImage = document.getElementById("card-image");
     const cardSrc = ['1-category.webp', '2-category.webp', '3-category.webp', '4-category.webp', '5-category.webp', '6-category.webp', '7-category.webp'];
 
     const cardCategory = document.getElementById("card-category");
     const cardQuestion = document.getElementById("card-question");
+
     cardCategory.innerHTML = '';
     cardQuestion.innerHTML = '';
 
-    let i = 0;
     const startTime = new Date().getTime();
-    setInterval(function(){
+    setInterval(function () {
+        let i = Math.floor(cardSrc.length * Math.random());
+
         if(new Date().getTime() - startTime > 4500){
             clearInterval;
             return;
         }
-        cardImage.setAttribute('src', 'img/'+ cardSrc[i++]);
-        if(i == cardSrc.length) i = 0;
+
+        cardImage.setAttribute('src', 'img/'+ cardSrc[i]);
     }, 140);
 }
 
-function getCards(){
+/*=============== GET ALL CARDS AND SAVE THEM TO LOCAL STORAGE ===============*/
+const getCards = () => {
     fetch("cards.json")
     .then(data => data.json())
     .then(data => {
@@ -92,7 +99,8 @@ function getCards(){
     });
 };
 
-function resetCards() {
+/*=============== RESET AVAILABLE CARDS FROM LOCAL STORAGE ===============*/
+const resetCards = () => {
     let confirmText = "Apakah Anda yakin untuk mengulangi permainan?";
     if (confirm(confirmText)) {
         fetch("cards.json")
