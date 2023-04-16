d2ef98c121ffd11128437d95b127f5c0388362fa
Fajar Ghifari <fajarghifari3@gmail.com>
2023-04-16 21:05:13 +0700

    build : make card generator working with javascript

---
+++ scripts/app.js
@@ -0,0 +1,80 @@
+function generateCard() {
+    const getLocalStorageData = localStorage.getItem("available_cards");
+
+    if (JSON.parse(getLocalStorageData).length == 0) {
+        const cardCategory = document.getElementById("card-category");
+        const cardQuestion = document.getElementById("card-question");
+        const cardImage = document.getElementById("card-image");
+        cardCategory.innerHTML = 'Tidak ada kartu yang tersisa!:(';
+        cardQuestion.innerHTML = 'Harap refresh halaman web untuk memulai ulang permainan!';
+        cardImage.src = 'img/default.webp';
+        console.log('Habiss')
+    } else {
+        const cards = JSON.parse(getLocalStorageData);
+        const randomIndex = Math.floor(cards.length * Math.random())
+
+        showCard(cards, cards[randomIndex]);
+    }
+}
+
+function showCard(cards, card) {
+    const cardTotal = document.getElementById("card-total");
+    const cardImage = document.getElementById("card-image");
+    const cardCategory = document.getElementById("card-category");
+    const cardQuestion = document.getElementById("card-question");
+
+    randomCardAnimation();
+
+    setTimeout(function(){
+        cardImage.src = 'img/' + card.img;
+        cardTotal.innerHTML = cards.length + '/60';
+        cardCategory.innerHTML = card.category;
+        cardQuestion.innerHTML = card.question;
+    }, 1800);
+
+    // Delete the card after openned
+    cards.splice(card, 1);
+    localStorage.setItem("available_cards", JSON.stringify(cards));
+}
+
+function randomCardAnimation(){
+    const cardImage = document.getElementById("card-image");
+    const cardSrc = ['1-category.webp', '2-category.webp', '3-category.webp', '4-category.webp', '5-category.webp', '6-category.webp', '7-category.webp'];
+
+    const cardCategory = document.getElementById("card-category");
+    const cardQuestion = document.getElementById("card-question");
+    cardCategory.innerHTML = '';
+    cardQuestion.innerHTML = '';
+
+    let i = 0;
+    const startTime = new Date().getTime();
+    setInterval(function(){
+        if(new Date().getTime() - startTime > 1800){
+            clearInterval;
+            return;
+        }
+        cardImage.setAttribute('src', 'img/'+ cardSrc[i++]);
+        if(i == cardSrc.length) i = 0;
+    }, 130);
+}
+
+const getCards = () => {
+    fetch("cards.json")
+    .then(data => data.json())
+    .then(data => {
+        let getLocalStorageData = localStorage.getItem("available_cards");
+
+        if (getLocalStorageData == null || JSON.parse(getLocalStorageData).length == 0) {
+            cards = data;
+        } else {
+            cards = JSON.parse(getLocalStorageData);
+        }
+
+        const cardTotal = document.getElementById("card-total");
+        cardTotal.innerHTML = cards.length + '/60';
+
+        localStorage.setItem("available_cards", JSON.stringify(cards));
+    });
+};
+
+window.addEventListener("load", getCards);
