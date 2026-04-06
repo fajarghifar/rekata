c67885d138d0a8bdc2284f1bf3f47f23a1785bca
fajarghifar <fajarghifari3@gmail.com>
2026-04-06 15:58:19 +0700

    init: setup react, vite, and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+      <h1 className="text-4xl font-bold text-slate-900 mb-4">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
